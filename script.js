1db4da54d73859f2a68dd87096fcf179d68fd495
Edward Chan <ed.ph.chan@gmail.com>
2020-10-03 20:09:49 -0600

    final tweek on js file

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ var confirmUppercase;
 var confirmLowercase;
 // Password variable values: 
 
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Special characters 
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // Alphabetical characters
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // large is for the Uppercase conversion
-large = [];
+caps = [];
 // Choices declared outside the if statement so they can be concatenated upon condition
 var choices;
 // converts letters to uppercase 
@@ -99,7 +99,7 @@ function generatePassword() {
     }
     // Created space variable to fill uppercase conversion
     else if (confirmUppercase) {
-        choices = large.concat(alpha2);
+        choices = caps.concat(alpha2);
     };
 
     // password variable
